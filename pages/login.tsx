73ef3c1b72e47cf44812fe491c21f68b04b867cf
Brayan-724 <bjqf0724@gmail.com>
2022-01-26 18:12:17 -0700

    UPDATE BRANCH VERCEL

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,15 @@
 import { Layout } from "~/app/types/page";
 import { page } from "~/app/utils/page";
 
-export default page(() => {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  );
-}, {
-  layout: Layout.None,
-})
+export default page(
+  () => {
+    return (
+      <div>
+        <h1>Login</h1>
+      </div>
+    );
+  },
+  {
+    layout: Layout.None,
+  }
+);
