6b7ba07e1d77e9804083e5bd08161b473351414f
Brayan-724 <bjqf0724@gmail.com>
2022-01-28 19:04:19 -0700

    Config to can get images from github avatars

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
 
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+
   serverRuntimeConfig: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
